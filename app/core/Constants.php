42dc6f90019fc2108df071626c1c3ab330fb43fd
zacky azmi asikin <zack@zackys-MacBook-Pro.local>
2024-10-21 00:19:53 +0700

    Membuat Aplikasi MVC dengan PHP #6 Assets

---
+++ app/core/Constants.php
@@ -0,0 +1,5 @@
+<?php 
+
+define('BASEURL', 'http://localhost:8888/prakweb2024_C_223040127_MVC/public');
+
+?>
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,7 @@
+<div class="container">
 
-<h1>Ini adalah Halaman About</h1>
+    <h1 class="mt-4">About Me</h1>
+    <img src="<?= BASEURL; ?>/img/zack.jpeg" alt="Zacky Azmi" width="150" class="rounded-circle shadow">
     <p>Nama Saya <?= $data['nama']; ?>, Umur Saya <?= $data['umur']; ?> Tahun, Saya Seorang <?= $data['pekerjaan']; ?></p>
+    
+</div>
