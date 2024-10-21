b8390d7b824d111e4b463f6e66679ad8c3b1d95b
zacky azmi asikin <zack@zackys-MacBook-Pro.local>
2024-10-21 13:34:27 +0700

    Membuat Aplikasi MVC dengan PHP #11 Delete Data

--- app/controllers/Mahasiswa.php
+++ app/controllers/Mahasiswa.php
@@ -32,6 +32,20 @@ class Mahasiswa extends Controller{
         }
     }
 
+    public function hapus($id)
+    {
+        if($this->model('Mahasiswa_model')->hapusDataMahasiswa($id) > 0){
+            Flasher::setFlash('berhasil', 'dihapus', 'success');
+            header('Location: '. BASEURL . '/mahasiswa');
+            exit;
+        } else{
+            Flasher::setFlash('gagal', 'dihapus', 'danger');
+            header('Location: '. BASEURL . '/mahasiswa');
+            exit;
+        }
+    }
+    
+
 }
 
 ?>
