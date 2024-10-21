5bc837cac3c40b32c3ac3aa7e571205e865bae53
zacky azmi asikin <zack@zackys-MacBook-Pro.local>
2024-10-21 12:59:11 +0700

    Membuat Aplikasi MVC dengan PHP #9 Insert Data

--- app/core/Database.php
+++ app/core/Database.php
@@ -72,6 +72,11 @@ class Database{
                 return $this->stmt->fetch(PDO::FETCH_ASSOC);
             }
 
+            public function rowCount()
+            {
+                return $this->stmt->rowCount();
+            }
+
 
 
 }
--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -1,7 +1,10 @@
-<div class="container mt-4">
+<div class="container mt-3">
 
     <div class="row">
         <div class="col-6">
+        <button type="button" class="btn btn-primary mb-2" data-toggle="modal" data-target="#formModal">
+            Tambah Data Mahasiswa
+        </button>
             <h3>Daftar Mahasiswa</h3>
             <ul class="list-group">
             <?php foreach ($data['mhs'] as $mhs) : ?>
@@ -16,4 +19,51 @@
         </div>
     </div>
 
+</div>
+
+
+<!-- Modal -->
+<div class="modal fade" id="formModal" tabindex="-1" role="dialog" aria-labelledby="judulModal" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="POST">
+        <div class="form-group">
+            <label for="nama">Nama</label>
+            <input type="text" class="form-control" id="nama" name="nama">
+        </div>
+        <div class="form-group">
+            <label for="nrp">NRP</label>
+            <input type="number" class="form-control" id="nrp" name="nrp">
+        </div>
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" class="form-control" id="email" name="email">
+        </div>
+        <div class="form-group">
+            <label for="jurusan">Jurusan</label>
+            <select class="form-control" id="jurusan" name="jurusan">
+            <option value="Teknik Informatika">Teknik Informatika</option>
+            <option value="Teknik Mesin">Teknik Mesin</option>
+            <option value="Teknik Lingkungan">Teknik Lingkungan</option>
+            <option value="Teknik Pangan">Teknik Pangan</option>
+            <option value="Teknik Planalogi">Teknik Planalogi</option>
+            <option value="Teknik Industris">Teknik Industris</option>
+            </select>
+        </div>
+        
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Tambah Data</button>
+            </form>
+      </div>
+    </div>
+  </div>
 </div>
